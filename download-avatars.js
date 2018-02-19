973d42e01cc6fe98a143d701fc02fe13d452b447
Dylanlj <dylanl-j@hotmail.ca>
2018-02-19 21:59:49 +0000

    finished step 7, printing results of avatar urls to console

--- download-avatars.js
+++ download-avatars.js
@@ -1,24 +1,32 @@
 var request = require("request");
 var token = require("./secrets")
-console.log(token.GITHUB_TOKEN)
-console.log('Welcome to the GitHub Avatar Downloader')
 
+console.log('Welcome to the GitHub Avatar Downloader')
 function getRepoContributors(repoOwner, repoName, cb) {
+//address and acess  
   var options = {
     url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-     'User-Agent': token.GITHUB_TOKEN
+     'User-Agent': 'request',
+     'Authorization': 'token' + token.GITHUB_TOKEN
     }
-  };
-  
+  }
 
   request(options, function(err, res, body) {
     cb(err, body);
-  });
+  })
+
+
+
 }
 
 //what we're giving the getRepoContributors function
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  result = JSON.parse(result)
+  //console.log(result)
+  for(let obj of result){
+    console.log('Avatar url: ' + obj.avatar_url);
+  }
+  
 });
