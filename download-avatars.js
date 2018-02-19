2a1e324d1552f4afc9a5a722de90ccdd0c3167dd
Dylanlj <dylanl-j@hotmail.ca>
2018-02-19 19:40:24 +0000

    finished step p6, added authorization token

--- download-avatars.js
+++ download-avatars.js
@@ -1,8 +1,20 @@
 var request = require("request");
+var token = require("./secrets")
+console.log(token.GITHUB_TOKEN)
 console.log('Welcome to the GitHub Avatar Downloader')
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+     'User-Agent': token.GITHUB_TOKEN
+    }
+  };
   
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 //what we're giving the getRepoContributors function
