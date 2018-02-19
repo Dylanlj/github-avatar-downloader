5ae4bebcf325dcd20b8fe0c7c31c9eaa4089ae74
Dylanlj <dylanl-j@hotmail.ca>
2018-02-19 22:52:29 +0000

    finished step 10, created user generatedvariables for url and throws program if not is given

--- download-avatars.js
+++ download-avatars.js
@@ -1,42 +1,43 @@
+
 var request = require("request");
 var token = require("./secrets");
 var fs = require('fs');
+var repoOwner = process.argv.slice(2 , 3);
+var repoName = process.argv.slice(3 , 4);
 
-console.log('Welcome to the GitHub Avatar Downloader')
+if(!repoName[0]){
+throw 'You need to submit both a repo owner and repo name!'
+}
 
-function getRepoContributors(repoOwner, repoName, cb) {
-//address and acess  
-  var options = {
-    url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-     'User-Agent': 'request',
-     'Authorization': 'token' + token.GITHUB_TOKEN
+
+  function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+      url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+       'User-Agent': 'request',
+       'Authorization': 'token' + token.GITHUB_TOKEN
+      }
     }
-  }
 
-  request(options, function(err, res, body) {
-    cb(err, body);
-  })
-}
-
-function downloadImageByURL(url, filePath) {
-  request.get(url)
-    .on('error', function (err){
-      throw err;
+    request(options, function(err, res, body) {
+      cb(err, body);
     })
-    .pipe(fs.createWriteStream(filePath))
-}
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  result = JSON.parse(result)
-  for(let obj of result){
-    downloadImageByURL( obj.avatar_url, `./avatars/${obj.login}.jpg`)
   }
-});
 
+  function downloadImageByURL(url, filePath) {
+    request.get(url)
+      .on('error', function (err){
+        throw err;
+      })
+      .pipe(fs.createWriteStream(filePath))
+  }
 
-
-
+  getRepoContributors(repoOwner, repoName, function(err, result) {
+    console.log("Errors:", err);
+    result = JSON.parse(result)
+    for(let obj of result){
+      downloadImageByURL( obj.avatar_url, `./avatars/${obj.login}.jpg`)
+    }
+  });
 
 
