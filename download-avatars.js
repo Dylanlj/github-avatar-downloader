33b92d3da81765723bcd61f5a1d0326b6db90908
Dylanlj <dylanl-j@hotmail.ca>
2018-02-19 22:16:17 +0000

    finished step 7, got the function that will download an image and save it to a file

--- download-avatars.js
+++ download-avatars.js
@@ -1,7 +1,10 @@
 var request = require("request");
-var token = require("./secrets")
+var token = require("./secrets");
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader')
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
 //address and acess  
   var options = {
@@ -16,17 +19,25 @@ function getRepoContributors(repoOwner, repoName, cb) {
     cb(err, body);
   })
 
-
-
 }
 
-//what we're giving the getRepoContributors function
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  result = JSON.parse(result)
-  //console.log(result)
-  for(let obj of result){
-    console.log('Avatar url: ' + obj.avatar_url);
-  }
+
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function (err){
+      throw err;
+    })
+    .pipe(fs.createWriteStream(filePath))
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./downloaded.jpg");
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   result = JSON.parse(result)
+//   for(let obj of result){
+//     console.log('Avatar url: ' + obj.avatar_url);
+//   }
   
-});
+// });
